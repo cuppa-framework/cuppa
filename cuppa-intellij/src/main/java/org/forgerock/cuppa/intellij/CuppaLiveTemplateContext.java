ed2978d488b36124484538770bc7d9db0e16c7b6
Joe Bandenburg <joe.bandenburg@forgerock.com>
2016-03-20 16:29:04 +0000

    Add live template context for Cuppa test classes

---
+++ cuppa-intellij/src/main/java/org/forgerock/cuppa/intellij/CuppaLiveTemplateContext.java
@@ -0,0 +1,57 @@
+/*
+ * Copyright 2016 ForgeRock AS.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.forgerock.cuppa.intellij;
+
+import com.intellij.codeInsight.AnnotationUtil;
+import com.intellij.codeInsight.template.JavaCodeContextType;
+import com.intellij.psi.PsiClass;
+import com.intellij.psi.PsiElement;
+import com.intellij.psi.PsiFile;
+import com.intellij.psi.util.PsiTreeUtil;
+import org.jetbrains.annotations.NotNull;
+
+/**
+ * Extension to provide a context for Cuppa live templates so that they are only
+ * available in statements in Cuppa test classes.
+ */
+public final class CuppaLiveTemplateContext extends JavaCodeContextType {
+    private final Statement statement = new Statement();
+
+    /**
+     * Constructs a new context.
+     */
+    public CuppaLiveTemplateContext() {
+        super("CUPPA_TEST_STATEMENT", "Cuppa test class", Statement.class);
+    }
+
+    @Override
+    public boolean isInContext(@NotNull PsiFile file, int offset) {
+        if (!statement.isInContext(file, offset)) {
+            return false;
+        }
+        return super.isInContext(file, offset);
+    }
+
+    @Override
+    protected boolean isInContext(@NotNull PsiElement element) {
+        PsiClass containingClass = PsiTreeUtil.getParentOfType(element, PsiClass.class);
+        if (containingClass != null) {
+            return AnnotationUtil.isAnnotated(containingClass, "org.forgerock.cuppa.Test", true);
+        }
+        return false;
+    }
+}
