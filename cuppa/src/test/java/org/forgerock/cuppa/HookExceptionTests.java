1153f587b86a2cb04bd2675e7ec31168bf1aee59
Joe Bandenburg <joe.bandenburg@forgerock.com>
2016-04-09 14:21:36 +0100

    Refactor HookExceptionTests to simplify data providers
    Passing in strings instead of functions makes it much easier to
    tell which tests are failing in the test report.

--- cuppa/src/test/java/org/forgerock/cuppa/HookExceptionTests.java
+++ cuppa/src/test/java/org/forgerock/cuppa/HookExceptionTests.java
@@ -18,7 +18,8 @@ package org.forgerock.cuppa;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
-import static org.forgerock.cuppa.Cuppa.*;
+import static org.forgerock.cuppa.Cuppa.describe;
+import static org.forgerock.cuppa.Cuppa.it;
 import static org.forgerock.cuppa.TestCuppaSupport.*;
 import static org.mockito.Mockito.*;
 
@@ -28,130 +29,78 @@ import java.util.Map;
 import java.util.function.BiConsumer;
 
 import org.forgerock.cuppa.functions.HookFunction;
-import org.forgerock.cuppa.functions.TestBlockFunction;
 import org.forgerock.cuppa.functions.TestFunction;
+import org.forgerock.cuppa.model.Hook;
+import org.forgerock.cuppa.model.HookType;
 import org.forgerock.cuppa.model.TestBlock;
 import org.forgerock.cuppa.reporters.Reporter;
 import org.mockito.ArgumentCaptor;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
 public class HookExceptionTests {
 
-    private static final Map<String, BiConsumer<String, HookFunction>> TEST_HOOKS =
+    private static final Map<String, BiConsumer<String, HookFunction>> HOOKS =
             new HashMap<String, BiConsumer<String, HookFunction>>() {
         {
             put("beforeEach", Cuppa::beforeEach);
             put("afterEach", Cuppa::afterEach);
-        }
-    };
-
-    private static final Map<String, BiConsumer<String, HookFunction>> BLOCK_HOOKS =
-            new HashMap<String, BiConsumer<String, HookFunction>>() {
-        {
             put("before", Cuppa::before);
             put("after", Cuppa::after);
         }
     };
 
-    private static final Map<String, BiConsumer<String, HookFunction>> ALL_HOOKS =
-            new HashMap<String, BiConsumer<String, HookFunction>>() {
-        {
-            putAll(TEST_HOOKS);
-            putAll(BLOCK_HOOKS);
-        }
-    };
+    private Reporter reporter;
 
-    @DataProvider
-    private Iterator<Object[]> testInTestHooks() {
-        return TEST_HOOKS.values().stream()
-                .map(f -> new Object[] {
-                        (TestBlockFunction) () -> f.accept("hook", () -> it("", TestFunction.identity())),
-                })
-                .iterator();
-    }
-
-    @Test(dataProvider = "testInTestHooks")
-    public void addingTestsInTestHookShouldThrowException(TestBlockFunction hookWithTest) {
-
-        //Given
-        Reporter reporter = mock(Reporter.class);
-        TestBlock rootBlock = defineTests(() -> {
-            describe("tests in hook", () -> {
-                hookWithTest.apply();
-                it("does not run the test", TestFunction.identity());
-            });
-        });
-
-        //When
-        runTests(rootBlock, reporter);
-
-        //Then
-        ArgumentCaptor<Throwable> captor = ArgumentCaptor.forClass(Throwable.class);
-        verify(reporter).testHookFail(eq(findHook(rootBlock, "hook")), anyListOf(TestBlock.class),
-                eq(findTest(rootBlock, "does not run the test")), anyListOf(TestBlock.class), captor.capture());
-        assertThat(captor.getValue())
-                .isExactlyInstanceOf(CuppaException.class)
-                .hasMessage("'it' may only be nested within a 'describe' or 'when' block");
-    }
-
-    @DataProvider
-    private Iterator<Object[]> testInBlockHooks() {
-        return BLOCK_HOOKS.values().stream()
-                .map(f -> new Object[] {
-                        (TestBlockFunction) () -> f.accept("hook", () -> it("", TestFunction.identity())),
-                })
-                .iterator();
-    }
-
-    @Test(dataProvider = "testInBlockHooks")
-    public void addingTestsInBlockHookShouldThrowException(TestBlockFunction hookWithTest) {
-
-        //Given
-        Reporter reporter = mock(Reporter.class);
-        TestBlock rootBlock = defineTests(() -> {
-            describe("tests in hook", () -> {
-                hookWithTest.apply();
-                it("does not run the test", TestFunction.identity());
-            });
-        });
-
-        //When
-        runTests(rootBlock, reporter);
-
-        //Then
-        ArgumentCaptor<Throwable> captor = ArgumentCaptor.forClass(Throwable.class);
-        verify(reporter).blockHookFail(eq(findHook(rootBlock, "hook")), anyListOf(TestBlock.class), captor.capture());
-        assertThat(captor.getValue())
-                .isExactlyInstanceOf(CuppaException.class)
-                .hasMessage("'it' may only be nested within a 'describe' or 'when' block");
-    }
-
-    @DataProvider
-    private Iterator<Object[]> hooks() {
-        return ALL_HOOKS.entrySet().stream()
-                .map(e -> new Object[]{
-                        e.getKey(),
-                        (Runnable) () -> e.getValue().accept("hook", HookFunction.identity()),
-                })
-                .iterator();
+    @BeforeMethod
+    public void setupMocks() {
+        reporter = mock(Reporter.class);
     }
 
     @Test(dataProvider = "hooks")
-    public void addingHookAtTopLevelShouldThrowException(String hookName, Runnable hook) {
-        assertThatThrownBy(() -> defineTests(hook::run))
+    public void addingTestsInHookShouldThrowException(String hookName) {
+
+        //Given
+        TestBlock rootBlock = defineTests(() -> {
+            describe("tests in hook", () -> {
+                addHook(hookName, "hook", () -> {
+                    it("test", TestFunction.identity());
+                });
+                it("does not run the test", TestFunction.identity());
+            });
+        });
+
+        //When
+        runTests(rootBlock, reporter);
+
+        //Then
+        Throwable cause = verifyHookFail(findHook(rootBlock, "hook"));
+        assertThat(cause)
+                .isExactlyInstanceOf(CuppaException.class)
+                .hasMessage("'it' may only be nested within a 'describe' or 'when' block");
+    }
+
+    @Test(dataProvider = "hooks")
+    public void addingHookAtTopLevelShouldThrowException(String hookName) {
+        assertThatThrownBy(() -> defineTests(() -> {
+            addHook(hookName, "hook", () -> {
+            });
+        }))
                 .isExactlyInstanceOf(CuppaException.class)
                 .hasMessage("'" + hookName + "' must be nested within a 'describe' or 'when' block");
     }
 
     @Test(dataProvider = "hooks")
-    public void addingHookInTestShouldThrowException(String hookName, Runnable hook) {
+    public void addingHookInTestShouldThrowException(String hookName) {
 
         //Given
-        Reporter reporter = mock(Reporter.class);
         TestBlock rootBlock = defineTests(() -> {
             describe("hook in test", () -> {
-                it("will cause the test to throw an error", hook::run);
+                it("will cause the test to throw an error", () -> {
+                    addHook(hookName, "hook", () -> {
+                    });
+                });
             });
         });
 
@@ -160,33 +109,24 @@ public class HookExceptionTests {
 
         //Then
         ArgumentCaptor<Throwable> captor = ArgumentCaptor.forClass(Throwable.class);
-        verify(reporter).testFail(eq(findTest(rootBlock, "will cause the test to throw an error")),
-                anyListOf(TestBlock.class), captor.capture());
+        verify(reporter).testFail(any(org.forgerock.cuppa.model.Test.class), anyListOf(TestBlock.class),
+                captor.capture());
         assertThat(captor.getValue())
                 .isExactlyInstanceOf(CuppaException.class)
                 .hasMessage("'" + hookName + "' may only be nested within a 'describe' or 'when' block");
     }
 
-    @DataProvider
-    private Iterator<Object[]> hooksInTestHooks() {
-        return TEST_HOOKS.values().stream()
-                .flatMap(f ->
-                        ALL_HOOKS.entrySet().stream().map(e -> new Object[] {
-                                e.getKey(),
-                                (TestBlockFunction) () -> f.accept("hook",
-                                        () -> e.getValue().accept("", HookFunction.identity())),
-                        }))
-                .iterator();
-    }
-
-    @Test(dataProvider = "hooksInTestHooks")
-    public void addingNestedHookInTestHookShouldThrowException(String hookName, TestBlockFunction nestedHook) {
+    @Test(dataProvider = "hookPairs")
+    public void addingNestedHookInHookShouldThrowException(String hookName, String otherHookName) {
 
         //Given
-        Reporter reporter = mock(Reporter.class);
         TestBlock rootBlock = defineTests(() -> {
             describe("nested hook in hook", () -> {
-                nestedHook.apply();
+                addHook(hookName, "hook", () -> {
+                    addHook(otherHookName, "otherHook", () -> {
+
+                    });
+                });
                 it("does not run the test", TestFunction.identity());
             });
         });
@@ -195,35 +135,23 @@ public class HookExceptionTests {
         runTests(rootBlock, reporter);
 
         //Then
-        ArgumentCaptor<Throwable> captor = ArgumentCaptor.forClass(Throwable.class);
-        verify(reporter).testHookFail(eq(findHook(rootBlock, "hook")), anyListOf(TestBlock.class),
-                eq(findTest(rootBlock, "does not run the test")), anyListOf(TestBlock.class), captor.capture());
-        assertThat(captor.getValue())
+        Throwable cause = verifyHookFail(findHook(rootBlock, "hook"));
+        assertThat(cause)
                 .isExactlyInstanceOf(CuppaException.class)
-                .hasMessage("'" + hookName + "' may only be nested within a 'describe' or 'when' block");
+                .hasMessage("'" + otherHookName + "' may only be nested within a 'describe' or 'when' block");
     }
 
-    @DataProvider
-    private Iterator<Object[]> hooksInBlockHooks() {
-        return BLOCK_HOOKS.values().stream()
-                .flatMap(f ->
-                        ALL_HOOKS.entrySet().stream().map(e -> new Object[] {
-                                e.getKey(),
-                                (TestBlockFunction) () -> f.accept("hook",
-                                        () -> e.getValue().accept("", HookFunction.identity())),
-                        }))
-                .iterator();
-    }
-
-    @Test(dataProvider = "hooksInBlockHooks")
-    public void addingNestedHookInBlockHookShouldThrowException(String hookName, TestBlockFunction nestedHook) {
+    @Test(dataProvider = "hooks")
+    public void shouldAllowHookToThrowCheckedException(String hookName) throws Exception {
 
         //Given
-        Reporter reporter = mock(Reporter.class);
+        Exception exception = new Exception();
         TestBlock rootBlock = defineTests(() -> {
-            describe("nested hook in hook", () -> {
-                nestedHook.apply();
-                it("does not run the test", TestFunction.identity());
+            describe("before blocks", () -> {
+                addHook(hookName, "hook", () -> {
+                    throw exception;
+                });
+                it("a test", TestFunction.identity());
             });
         });
 
@@ -231,130 +159,54 @@ public class HookExceptionTests {
         runTests(rootBlock, reporter);
 
         //Then
+        Throwable cause = verifyHookFail(findHook(rootBlock, "hook"));
+        assertThat(cause).isSameAs(exception);
+    }
+
+    @Test(dataProvider = "hooks")
+    public void shouldAllowHookToThrowThrowable(String hookName) throws Exception {
+
+        //Given
+        AssertionError error = new AssertionError();
+        TestBlock rootBlock = defineTests(() -> {
+            describe("before blocks", () -> {
+                addHook(hookName, "hook", () -> {
+                    throw error;
+                });
+                it("a test", TestFunction.identity());
+            });
+        });
+
+        //When
+        runTests(rootBlock, reporter);
+
+        //Then
+        Throwable cause = verifyHookFail(findHook(rootBlock, "hook"));
+        assertThat(cause).isSameAs(error);
+    }
+
+    @DataProvider
+    private Iterator<Object[]> hooks() {
+        return HOOKS.keySet().stream().map(i -> new Object[]{i}).iterator();
+    }
+
+    @DataProvider
+    private Iterator<Object[]> hookPairs() {
+        return HOOKS.keySet().stream().flatMap(a -> HOOKS.keySet().stream().map(b -> new Object[]{a, b})).iterator();
+    }
+
+    private void addHook(String hookName, String description, HookFunction function) {
+        HOOKS.get(hookName).accept(description, function);
+    }
+
+    private Throwable verifyHookFail(Hook hook) {
         ArgumentCaptor<Throwable> captor = ArgumentCaptor.forClass(Throwable.class);
-        verify(reporter).blockHookFail(eq(findHook(rootBlock, "hook")), anyListOf(TestBlock.class), captor.capture());
-        assertThat(captor.getValue())
-                .isExactlyInstanceOf(CuppaException.class)
-                .hasMessage("'" + hookName + "' may only be nested within a 'describe' or 'when' block");
-    }
-
-    @DataProvider
-    private Iterator<Object[]> testHooksThrowExceptions() {
-        return TEST_HOOKS.values().stream()
-                .map(f -> (TestBlockFunction) () -> f.accept("hook", () -> {
-                    throw new Exception();
-                }))
-                .map(f -> new Object[]{f})
-                .iterator();
-    }
-
-    @Test(dataProvider = "testHooksThrowExceptions")
-    public void shouldAllowTestHookToThrowCheckedException(TestBlockFunction hook) throws Exception {
-
-        //Given
-        Reporter reporter = mock(Reporter.class);
-        TestBlock rootBlock = defineTests(() -> {
-            describe("before blocks", () -> {
-                hook.apply();
-                it("a test", TestFunction.identity());
-            });
-        });
-
-        //When
-        runTests(rootBlock, reporter);
-
-        //Then
-        verify(reporter).testHookFail(eq(findHook(rootBlock, "hook")), anyListOf(TestBlock.class),
-                eq(findTest(rootBlock, "a test")), anyListOf(TestBlock.class), isA(Exception.class));
-    }
-
-    @DataProvider
-    private Iterator<Object[]> blockHooksThrowExceptions() {
-        return BLOCK_HOOKS.values().stream()
-                .map(f -> (TestBlockFunction) () -> f.accept("hook", () -> {
-                    throw new Exception();
-                }))
-                .map(f -> new Object[]{f})
-                .iterator();
-    }
-
-    @Test(dataProvider = "blockHooksThrowExceptions")
-    public void shouldAllowBlockHookToThrowCheckedException(TestBlockFunction hook) throws Exception {
-
-        //Given
-        Reporter reporter = mock(Reporter.class);
-        TestBlock rootBlock = defineTests(() -> {
-            describe("before blocks", () -> {
-                hook.apply();
-                it("a test", TestFunction.identity());
-            });
-        });
-
-        //When
-        runTests(rootBlock, reporter);
-
-        //Then
-        verify(reporter).blockHookFail(eq(findHook(rootBlock, "hook")), anyListOf(TestBlock.class),
-                isA(Exception.class));
-    }
-
-    @DataProvider
-    private Iterator<Object[]> testHooksThrowThrowable() {
-        return TEST_HOOKS.values().stream()
-                .map(f -> (TestBlockFunction) () -> f.accept("hook", () -> {
-                    throw new AssertionError();
-                }))
-                .map(f -> new Object[]{f})
-                .iterator();
-    }
-
-    @Test(dataProvider = "testHooksThrowThrowable")
-    public void shouldAllowTestHookToThrowThrowable(TestBlockFunction hook) throws Exception {
-
-        //Given
-        Reporter reporter = mock(Reporter.class);
-        TestBlock rootBlock = defineTests(() -> {
-            describe("before blocks", () -> {
-                hook.apply();
-                it("a test", TestFunction.identity());
-            });
-        });
-
-        //When
-        runTests(rootBlock, reporter);
-
-        //Then
-        verify(reporter).testHookFail(eq(findHook(rootBlock, "hook")), anyListOf(TestBlock.class),
-                eq(findTest(rootBlock, "a test")), anyListOf(TestBlock.class), isA(AssertionError.class));
-    }
-
-    @DataProvider
-    private Iterator<Object[]> blockHooksThrowThrowable() {
-        return BLOCK_HOOKS.values().stream()
-                .map(f -> (TestBlockFunction) () -> f.accept("hook", () -> {
-                    throw new AssertionError();
-                }))
-                .map(f -> new Object[]{f})
-                .iterator();
-    }
-
-    @Test(dataProvider = "blockHooksThrowThrowable")
-    public void shouldAllowBlockHookToThrowThrowable(TestBlockFunction hook) throws Exception {
-
-        //Given
-        Reporter reporter = mock(Reporter.class);
-        TestBlock rootBlock = defineTests(() -> {
-            describe("before blocks", () -> {
-                hook.apply();
-                it("a test", TestFunction.identity());
-            });
-        });
-
-        //When
-        runTests(rootBlock, reporter);
-
-        //Then
-        verify(reporter).blockHookFail(eq(findHook(rootBlock, "hook")), anyListOf(TestBlock.class),
-                isA(AssertionError.class));
+        if (hook.type == HookType.BEFORE_EACH || hook.type == HookType.AFTER_EACH) {
+            verify(reporter).testHookFail(eq(hook), anyListOf(TestBlock.class),
+                    any(org.forgerock.cuppa.model.Test.class), anyListOf(TestBlock.class), captor.capture());
+        } else {
+            verify(reporter).blockHookFail(eq(hook), anyListOf(TestBlock.class), captor.capture());
+        }
+        return captor.getValue();
     }
 }
